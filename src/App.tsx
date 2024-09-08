62cbb54b6ea79234fc2940de898cb9689d8c9568
mvacoimbra <mvacoimbra.dev@gmail.com>
2024-09-08 17:54:21 -0300

    chore: config main files

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import Navbar from './components/navbar'
+import About from './sections/about'
+import Hero from './sections/hero'
+
+library.add(fab, fas, far)
+
 export default function App() {
   return (
-    <div>
-      <h1 className="">Hello</h1>
-    </div>
+    <>
+      <Navbar />
+      <Hero />
+      <About />
+    </>
   )
 }
