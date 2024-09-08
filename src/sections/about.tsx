0529de8486506275ebdc1e9db4a1da0c3c88f29a
mvacoimbra <mvacoimbra.dev@gmail.com>
2024-09-08 17:54:08 -0300

    feat: add test sections

---
+++ src/sections/about.tsx
@@ -0,0 +1,11 @@
+import Section from '../components/section'
+
+export default function About() {
+  return (
+    <Section className="hero-bg h-svh" id="about">
+      <div className="flex h-full w-full items-center justify-center text-on-yellow">
+        <h3 className="text-6xl">Sobre</h3>
+      </div>
+    </Section>
+  )
+}
---
+++ src/sections/hero.tsx
@@ -0,0 +1,13 @@
+import Section from '../components/section'
+
+export default function Hero() {
+  return (
+    <Section className="hero-bg h-svh" id="hero">
+      <div className="text-on-yellow">
+        <h3 className="text-6xl">Inspire-se,</h3>
+        <h2 className="text-8xl">aprenda,</h2>
+        <h1 className="text-9xl">evolua.</h1>
+      </div>
+    </Section>
+  )
+}
