a92939bd25f5083e8f4e4c22927052e618e76706
mvacoimbra <mvacoimbra.dev@gmail.com>
2024-09-08 18:26:52 -0300

    build: update build config

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import App from './app.tsx'
+import App from './app'
 
 const rootContainer = document.getElementById('root') as HTMLElement
 ReactDOM.createRoot(rootContainer).render(
