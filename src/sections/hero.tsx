b1ab8d6a1828ea20a2fc082fa8242304e6a247a1
mvacoimbra <mvacoimbra.dev@gmail.com>
2024-09-10 01:41:00 -0300

    refactor: update hero section

--- src/sections/hero.tsx
+++ src/sections/hero.tsx
@@ -4,19 +4,19 @@ export default function Hero() {
   return (
     <Section className="hero-bg h-lvh" id="hero">
       <img
-        src="hero-img.png"
+        src="/images/hero-img.png"
         alt="hero-img"
         className="absolute top-0 right-0 h-[90%] max-xl:hidden"
       />
       <div className="flex h-full flex-col items-start justify-center">
-        <div className="flex mt-12 w-1/2 text-on-white max-lg:gap-7 gap-7 max-sm:flex-col lg:flex-col max-lg:w-full">
+        <div className="mt-12 flex w-1/2 gap-7 text-on-white max-sm:flex-col max-lg:w-full max-lg:gap-7 lg:flex-col">
           <div>
-            <h3 className="lg:text-6xl text-5xl">Inspire-se,</h3>
-            <h2 className="lg:text-8xl text-7xl">aprenda,</h2>
-            <h1 className="lg:text-9xl text-on-yellow text-8xl">evolua.</h1>
+            <h3 className="text-5xl lg:text-6xl">Inspire-se,</h3>
+            <h2 className="text-7xl lg:text-8xl">aprenda,</h2>
+            <h1 className="text-8xl text-on-yellow lg:text-9xl">evolua.</h1>
           </div>
 
-          <div className="w-full h-1 bg-on-white max-lg:h-full max-lg:w-3 max-sm:w-full max-sm:h-1" />
+          <div className="h-1 w-full bg-on-white max-sm:h-1 max-sm:w-full max-lg:h-full max-lg:w-3" />
 
           <p className=" text-lg">
             Descubra a magia da música com a Escola de Música On! Sob a direção
