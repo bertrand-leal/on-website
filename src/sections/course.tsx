5ef5fbb07341012629184f81d16e95ddaa09a30f
mvacoimbra <mvacoimbra.dev@gmail.com>
2024-09-10 01:45:03 -0300

    chore: add section id

--- src/sections/course.tsx
+++ src/sections/course.tsx
@@ -2,7 +2,7 @@ import Section from '@src/components/section'
 
 export default function Course() {
   return (
-    <Section className="h-svh" hasTopDivider>
+    <Section className="h-svh" hasTopDivider id="course">
       <div className="flex h-full flex-col items-center justify-center">
         <h4 className="text-5xl text-on-yellow lg:text-6xl">
           Método Tecladista Completo
