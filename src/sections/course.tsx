33e1d5dca2485cce155ea360eccb8066d88efadb
mvacoimbra <mvacoimbra.dev@gmail.com>
2024-09-10 01:41:28 -0300

    feat: add course section prev

---
+++ src/sections/course.tsx
@@ -0,0 +1,13 @@
+import Section from '@src/components/section'
+
+export default function Course() {
+  return (
+    <Section className="h-svh" hasTopDivider>
+      <div className="flex h-full flex-col items-center justify-center">
+        <h4 className="text-5xl text-on-yellow lg:text-6xl">
+          Método Tecladista Completo
+        </h4>
+      </div>
+    </Section>
+  )
+}
