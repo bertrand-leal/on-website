b348d9f2cf077bc41ea2b0d8824ec48cf5ab999a
mvacoimbra <mvacoimbra.dev@gmail.com>
2024-09-10 01:41:48 -0300

    build: update config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
+import path from 'node:path'
 import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@assets': path.resolve(__dirname, './public/assets'),
+      '@src': path.resolve(__dirname, './src'),
+    },
+  },
   build: {
     rollupOptions: {
       output: {
